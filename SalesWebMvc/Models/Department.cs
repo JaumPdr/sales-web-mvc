74fd4032d00f7cc860dbf5f375b73d67c7132b71
JaumPdr <oliveirap2002@gmail.com>
2026-03-27 10:08:11 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,10 +1,37 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // Para cada vendedor (Seller) na coleção Sellers,
+            // chama o método TotalSales(initial, final),
+            // que calcula o total de vendas dele no período informado.
+            // Em seguida, soma o resultado de todos os vendedores,
+            // retornando o total geral de vendas no intervalo.
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
