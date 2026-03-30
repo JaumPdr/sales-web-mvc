27697b0474555d299bff969da465d272fd241b5e
JaumPdr <oliveirap2002@gmail.com>
2026-03-30 15:27:26 -0300

    App locale, number and date formatting

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -9,10 +10,24 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        // Define o formato de exibição da data no padrão brasileiro (dia/mês/ano)
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        // Especifica que o campo deve ser tratado como um tipo de dado Data (sem horário)
+        [DataType(DataType.Date)]
+        // Define o nome que será exibido na interface (label do campo)
+        [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Display(Name = "Base Salary")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+
+        [Display(Name = "Departments")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
